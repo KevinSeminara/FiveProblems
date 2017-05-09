58e992d107cc429c66dd81c1073c141789d27f2a
kevinseminara <kevinseminara@gmail.com>
2017-05-09 15:13:39 -0700

    Add p2

---
+++ p2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+print "Hello p2!"
+
+l1 = ['a', 'b', 'c']
+l2 = ['1', '2', '3']
+output = []
+
+for x in range(len(l1)):
+    output.append(l1[x])
+    output.append(l2[x])
+
+
+print output
+
