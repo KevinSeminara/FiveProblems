278d9378e6e4d7c997a5222f462734ddba34237c
kevinseminara <kevinseminara@gmail.com>
2017-05-10 15:59:52 -0700

    finished p4

--- p4.py
+++ p4.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
-import sys
 import itertools
 
 print "Hello Problem 4"
 
-input = {5, 56, 50}
+input = [420, 34, 19, 71, 341]
 
 perm = list(itertools.permutations(input))
+finalperm = []
+
+for each in perm:
+    s = map(str, each)
+    s = ''.join(s)
+    s = int(s)
+    finalperm.append(s)
+
+finalperm.sort()
+
+print "Smallest number is: %d" % finalperm[0]
+print "Largest number is: %d" % finalperm[-1]
+
 
